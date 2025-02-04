a7de1fecb21bfa2ca0013e964e8a3c01b2b580fa
Sourav Kumar Sheel <souravkrsheel99@gmail.com>
2025-02-04 23:39:23 +0530

    randomForest

---
+++ RandomForest/randomForestClassifier.py
@@ -0,0 +1,7 @@
+# Import necessary libraries
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
